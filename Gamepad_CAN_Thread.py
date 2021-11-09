24a7d36e426bd99f21010e0722a4afa38c8a7ad4
IsaacSinn <>
2021-11-09 19:23:48 +0800

    Update Gamepad_CAN_Thread.py
    -Need to test the module

--- Gamepad_CAN_Thread.py
+++ Gamepad_CAN_Thread.py
@@ -1,12 +1,50 @@
 import pygame
+import threading
 from threading import Thread
 from pubsub import pub
+import time
+import can
+import at_serial_can
 
 
-class joystick:
+class joystick(threading.Thread):
+
     def __init__(self):
-        pygame.joystick.JoyStick(0)
-        joystick.init()
+        pygame.init()
+        pygame.joystick.init()
+
+        try:
+            self.joystick = pygame.joystick.Joystick(0)
+        except:
+            raise TypeError("No joystick connected")
+        self.joystick.init()
+
+        threading.Thread.__init__(self.get_joystick(), daemon=True)
+
+    def get_joystick(self):
+        while True:
+            pygame.event.pump()
+            for i in range(self.joystick.get_numaxes()):
+                self.direct_input[i] = self.joystick.get_axis(i)
+
+            pub.sendMessage("joystick.movement", message = {"joystick.movement": self.direct_input})
+
+class CAN_Handler(threading.Thread):
+
+    def __init__(self):
+        pub.subscribe(self.message_listener, "joystick.movement")
+        self.bus = at_serial_can.ATSerialBus(channel = "COM7", bitrate=250000)
+        threading.Thread.__init__(self.receive(), dameon=True)
+
+    def receive(self):
+        msg = self.bus.recv(0)
+        print(msg)
 
 
-    def joystick_run():
+
+if __name__ == '__main__':
+    joystick = joystick()
+    joystick.start()
+
+    CAN_Handler = CAN_Handler()
+    CAN_Handler.start()
