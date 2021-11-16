5bccd0dbffd13b1c1f0761d0b7f83dbce97bc92d
IsaacSinn <>
2021-11-16 18:48:05 +0800

    Update Gamepad_CAN_Thread.py
    - test the program tomorrow with thruster and xbox controller

--- Gamepad_CAN_Thread.py
+++ Gamepad_CAN_Thread.py
@@ -12,6 +12,8 @@ class joystick(threading.Thread):
     def __init__(self):
         pygame.init()
         pygame.joystick.init()
+        self.direct_input = [0 for i in range(6)]
+        self.output_power = 0
 
         try:
             self.joystick = pygame.joystick.Joystick(0)
@@ -26,19 +28,30 @@ class joystick(threading.Thread):
             pygame.event.pump()
             for i in range(self.joystick.get_numaxes()):
                 self.direct_input[i] = self.joystick.get_axis(i)
+                LUD = self.direct_input[1]
 
-            pub.sendMessage("joystick.movement", message = {"joystick.movement": self.direct_input})
+            if LUD >= 0:
+                self.output_power = int(LUD*32767)
+            else:
+                self.output_power = int(LUD*32768)
+
+            #print(self.output_power)
+            pub.sendMessage("can.send", message = {"address": 0xFF, "data": [32, self.output_power >> 8 & 0xFF, self.output_power & 0xFF]})
 
 class CAN_Handler(threading.Thread):
 
     def __init__(self):
-        pub.subscribe(self.message_listener, "joystick.movement")
-        self.bus = at_serial_can.ATSerialBus(channel = "COM7", bitrate=250000)
+        pub.subscribe(self.message_listener, "can.send")
+        self.bus = at_serial_can.ATSerialBus(channel = "COM3", bitrate=250000)
         threading.Thread.__init__(self.receive(), dameon=True)
+
+    def message_listener(self, message):
+        msg  = can.Message(arbitration_id = message["address"], data = message["data"], is_extedned_id = False)
+        self.bus.send(msg)
 
     def receive(self):
         msg = self.bus.recv(0)
-        print(msg)
+        print("can bus received: ", msg)
 
 
 
@@ -47,4 +60,4 @@ if __name__ == '__main__':
     joystick.start()
 
     CAN_Handler = CAN_Handler()
-    CAN_Handler.start()
+    #CAN_Handler.start()
