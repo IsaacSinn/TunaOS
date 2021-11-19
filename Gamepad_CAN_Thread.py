83a95565ed190223d502192cba774e9f46833071
IsaacSinn <>
2021-11-19 19:55:07 +0800

    (Not Done) GUI class to test Threading

--- Gamepad_CAN_Thread.py
+++ Gamepad_CAN_Thread.py
@@ -3,7 +3,6 @@ import threading
 from threading import Thread
 from pubsub import pub
 import time
-import time
 import can
 import at_serial_can
 
@@ -15,7 +14,40 @@ class pygameLoop(threading.Thread):
         pygame.init()
         while True:
             pygame.event.pump()
-            time.sleep(0.01)
+            time.sleep(0.1)
+
+
+class gui(threading.Thread):
+    def __init__(self):
+        super(gui, self).__init__()
+        pygame.display.set_caption("GUI")
+        self.screen = pygame.display.set_mode((800, 600))
+
+        self.background = pygame.Surface((800, 600))
+        self.background.fill(pygame.Color('#00FF00'))
+
+        self.is_running = True
+
+        pygame.font.init()
+        self.font = pygame.font.SysFont("Comic Sans MS", 30)
+
+    def run(self):
+        while self.is_running:
+            print("running")
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_running = False
+
+            text = self.font.render(f"", False, (0,0,0))
+
+            self.screen.blit(self.background, (0, 0))
+            self.screen.blit(text, (0,0))
+
+            pygame.display.update()
+
+
+
 
 class joystick(threading.Thread):
 
@@ -35,12 +67,10 @@ class joystick(threading.Thread):
     def run(self):
         while True:
             time.sleep(0.1)
-            #pygame.event.pump()
             for i in range(self.joystick.get_numaxes()):
                 self.direct_input[i] = self.joystick.get_axis(i)
                 LUD = self.direct_input[1]
-            print(self.direct_input)
-            '''
+
             if LUD >= 0:
                 self.output_power = int(LUD*32767)
             else:
@@ -48,7 +78,7 @@ class joystick(threading.Thread):
 
             print("output_power: " , self.output_power)
             pub.sendMessage("can.send", message = {"address": 0xFF, "data": [32, self.output_power >> 8 & 0xFF, self.output_power & 0xFF]})
-            '''
+
 class CAN_Handler(threading.Thread):
 
     def __init__(self):
@@ -59,40 +89,26 @@ class CAN_Handler(threading.Thread):
     def message_listener(self, message):
         msg  = can.Message(arbitration_id = message["address"], data = message["data"], is_extended_id = False)
         print("msg sent:", msg)
-        #self.bus.send(msg)
+        self.bus.send(msg)
 
     def run(self):
         while True:
             time.sleep(0.1)
             print("msg received")
-            #msg = self.bus.recv(0)
+            msg = self.bus.recv(0)
             #print("can bus received: ", msg)
 
-# class MyThread(threading.Thread):
-#     def __init__(self, name):
-#         super(MyThread,self).__init__()
-#         self.name = name
-#     def run(self):
-#         while True:
-#             print(self.name)
-#             time.sleep(0.6)
-
 if __name__ == '__main__':
-    pl = pygameLoop()
-
-    pl.start()
+    pygameloop = pygameLoop()
+    pygameloop.start()
 
     time.sleep(1)
 
-
-    #CAN_Handler = CAN_Handler()
+    CAN_Handler = CAN_Handler()
     joystick = joystick()
 
-    #CAN_Handler.start()
+    CAN_Handler.start()
     joystick.start()
 
-    # a = MyThread("A")
-    # b = MyThread("B")
-    #
-    # a.start()
-    # b.start()
+    gui = gui()
+    gui.start()
