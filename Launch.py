387668c47604fe24299bc29d9f6c964219ba7f39
IsaacSinn <>
2022-01-11 18:55:06 +0800

    Added ThrusterPower.py and Thruster.yaml

--- Launch.py
+++ Launch.py
@@ -5,6 +5,8 @@ from PyGameServices import PyGameServices
 # Modules
 from GUI import GUI
 from Joystick import Joystick
+from ControlProfile import ControlProfile
+from ThrusterPower import ThrusterPower
 
 mm = ModuleManager()
 pygs = PyGameServices()
@@ -12,6 +14,11 @@ pygs.start(60)
 
 GUI = GUI()
 Joystick = Joystick()
+ControlProfileA = ControlProfile(100, 30, "A")
+ControlProfileB = ControlProfile(70, 50, "B")
+ControlProfileC = ControlProfile(50, 50, "C")
+ControlProfileD = ControlProfile(30, 50, "D")
+ThrusterPower = ThrusterPower()
 
-mm.register((GUI, 60), (Joystick, 60))
+mm.register((Joystick, 120), (ControlProfileA, 60), (ControlProfileB, 60), (ControlProfileC, 60), (ControlProfileD, 60), (ThrusterPower, 60))
 mm.start_all()
