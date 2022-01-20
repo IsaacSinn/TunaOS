4856b198b056aa107aa2f0f713f72bf80b0285e7
hftchan <70085368+hftchan@users.noreply.github.com>
2022-01-20 23:29:34 +0800

    E.M. Background and Gripper Assets added

--- GUI.py
+++ GUI.py
@@ -9,6 +9,7 @@ class GUI(Module):
         # variables
         self.movement = [0 for i in range(6)]
         self.mode = (800, 800)
+        self.gripper = 1
 
         # request from PyGameServices
         pygs = PyGameServices()
@@ -23,6 +24,7 @@ class GUI(Module):
         self.dark_skin = (226, 185, 143)
         self.dark_red = (113,2,0)
         self.turquoise = '#9cc3cd'
+        self.blue = '#1F456E'
 
 
         self.background = self.pygame.Surface(self.mode)
@@ -39,12 +41,17 @@ class GUI(Module):
 
         # pubsub init
         pub.subscribe(self.direct_handler, "gamepad.direct")
+        pub.subscribe(self.gripper_handler,"gamepad.gripper")
 
 
 
     # pubsub handler
     def direct_handler(self, message):
         self.movement = message["gamepad_direct"]
+
+    def gripper_handler(self, message):
+        self.gripper = message["Tool_state"]
+
 
     def run(self):
         text = self.font.render(f"{self.movement}", False, (0,0,0))
@@ -65,6 +72,11 @@ class GUI(Module):
 
         def rect(colour, dimensions,):
             self.pygame.draw.rect(self.screen,colour, self.pygame.Rect(dimensions))
+            #(X coord, Y position from top, length, thickness)
+
+        def triangle(colour,points):
+            self.pygame.draw.polygon(surface=self.screen, color=colour, points=points)
+
 
         def plottings(point_x, point_y, offset_x, offset_y):
             coord_x = point_x * 80 + offset_x
@@ -72,11 +84,52 @@ class GUI(Module):
             dcircle((0,200,0),(coord_x,coord_y),20)
 
         def em_back():
-            rect(self.yellow,50,100,200,50)
+            #large rectangles
+            rect(self.yellow,(100,350,200,100))
+            rect(self.blue,(400,350,200,100))
+
+            #top decorations
+            rect(self.yellow,(120,340,50,10))
+            rect(self.yellow,(230,340,50,10))
+
+            rect(self.blue,(420,340,50,10))
+            rect(self.blue,(530,340,50,10))
+
+            #triangle
+            triangle(self.black, [(230, 370), (230, 430), (270, 400)])
+            triangle(self.black, [(530, 370), (530, 430), (570, 400)])
+            triangle(self.black, [(170, 370), (170, 430), (130, 400)])
+            triangle(self.black, [(470, 370), (470, 430), (430, 400)])
+
+
+        def em_active():
+            print("OK")
+
 
         dots_back()
         plottings(LLR,-LUD,200,200)
         plottings(RLR,-RUD,500,200)
+
+        em_back()
+        print(self.gripper)
+
+        #gripper image assets
+        gripper_half = self.pygame.image.load(r'C:\Users\tonyc\OneDrive\Desktop\Documents\GitHub\TunaOS\GUI Assets\Gripper  Half Open.png')
+        gripper_half = self.pygame.transform.scale(gripper_half,(400,400))
+        gripper_closed = self.pygame.image.load(r'C:\Users\tonyc\OneDrive\Desktop\Documents\GitHub\TunaOS\GUI Assets\Gripper Closed.png')
+        gripper_closed = self.pygame.transform.scale(gripper_closed,(400,4000))
+        gripper_full_opened =self.pygame.image.load(r"C:\Users\tonyc\OneDrive\Desktop\Documents\GitHub\TunaOS\GUI Assets\Gripper fully opened.png")
+        gripper_full_opened = self.pygame.transform.scale(gripper_full_opened,(400,400))
+
+        if self.gripper == -1:
+            self.screen.blit(gripper_closed,(0,500))
+        elif self.gripper == 0:
+            self.screen.blit(gripper_half, (0, 500))
+        elif self.gripper == 1:
+            self.screen.blit(gripper_full_opened, (0, 500))
+
+
+
 
 
 
