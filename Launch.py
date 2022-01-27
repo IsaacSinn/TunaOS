c862b2aae1e13f62e3d2f1bf60c38b878b771ca3
IsaacSinn <sinweitengisaac@gmail.com>
2022-01-27 18:52:06 +0800

    Tested GUI frame rate fix; Joystick, EM and Gripper functional

--- Launch.py
+++ Launch.py
@@ -15,17 +15,17 @@ from Logger import Logger
 
 mm = ModuleManager()
 pygs = PyGameServices()
-pygs.start(60)
+pygs.start(120)
 
 GUI = GUI()
-# Joystick = Joystick()
+Joystick = Joystick()
 ControlProfileA = ControlProfile(100, 30, "A")
 ControlProfileB = ControlProfile(70, 50, "B")
 ControlProfileC = ControlProfile(50, 50, "C")
 ControlProfileD = ControlProfile(30, 50, "D")
 ThrusterPower = ThrusterPower()
 Thrusters = Thrusters()
-CANHandler = CANHandler()
+# CANHandler = CANHandler()
 # Logger = Logger(False, True, None, "log.sent") # FILE, PRINT, RATE_LIMITER, TOPICS
 
 # TOOLS
@@ -35,15 +35,15 @@ Gripper = Gripper("gripper", "0x22", "10000") # SPEED 0 - 32767
 
 # REGISTERING MODULES (INSTANCE, REFRESH PER SECOND)
 mm.register(
-#            (GUI, 60),
-#            (Joystick, 120),
+            (GUI, 60),
+            (Joystick, 120),
             (ControlProfileA, 60),
             (ControlProfileB, 60),
             (ControlProfileC, 60),
             (ControlProfileD, 60),
             (ThrusterPower, 60),
             (Thrusters, 60),
-            (CANHandler, 60),
+#            (CANHandler, 60),
             (EM1, 1),
             (EM2, 1),
             (Gripper, 1)
